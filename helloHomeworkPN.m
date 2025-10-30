9fd070d2284d461e0fa513baad2d19e5db987fcd
Pauline <pauline.nagel@stud.plus.ac.at>
2025-10-30 13:20:20 +0100

    silly cange 2

--- helloHomeworkPN.m
+++ helloHomeworkPN.m
@@ -5,3 +5,12 @@ disp('Hello World!')
 disp('Hello Homeowork!')
 
 a = 1
+
+% Some silly change 1
+b = a + 1
+% Some silly change 2
+c = b + 2;
+disp(['The value of c is: ', num2str(c)]);
+
+% Even sillier change
+%
